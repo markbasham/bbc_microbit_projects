13b60574a70cc2c756b30602297bad21c889c97f
Mark Basham <mark.basham@diamond.ac.uk>
2016-08-28 15:02:59 +0100

    Clean Up

--- simplelogger.py
+++ simplelogger.py
@@ -1,32 +1,30 @@
 from microbit import *
-count = 0
 
-# main loop
-while True: 
-    display.show(Image.ARROW_W)
-    
-    # wait for button a to be pressed
-    while True:
-        if button_a.is_pressed():
-            break
-    
-    display.show(Image.ARROW_E)
-    start_time = running_time()
-    
-    # open the next file
-    count+=1
-    with open('out%03i.csv' % (count), 'w') as my_file:
-        # write the file headder
-        my_file.write("time, x, y, z\n")
+POLL_TIME = 10
+MAX_FRAMES = 100
 
-        while True:
-            # check to see if b is pressed, if so exit
-            if button_b.is_pressed():
-                break
-        
-            # otherwise, log data
+# main loop
+while True: 
+    display.show(Image.ARROW_W)
+
+    # wait for button a to be pressed
+    while True:
+        if button_a.is_pressed():
+            break
+
+    display.show(Image.SMILE)
+    start_time = running_time()
+
+    # open the file
+    with open('output.csv', 'w') as my_file:
+        # write the file headder
+        my_file.write("time, x, y, z\n")
+        
+        count = 0
+        while count < MAX_FRAMES:
+            count+=1
+            # log data
             x_val = accelerometer.get_x()
             y_val = accelerometer.get_y()
-            z_val = accelerometer.get_z()
-            my_file.write("%f, %f, %f, %f\n" % ((running_time()-start_time), x_val, y_val, z_val))
-            sleep(100)
+            z_val = accelerometer.get_z()
+            my_file.write("%i, %i, %i, %i\n" % ((running_time()-start_time), x_val, y_val, z_val))
